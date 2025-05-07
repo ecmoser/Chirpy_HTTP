1c1f2366722da8d2c7c80fb6c27a64dc65ae007c
ecmoser <78171469+ecmoser@users.noreply.github.com>
2025-05-07 17:42:52 -0400

    added handler for health check

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	const filepathRoot = "./"
+	const filepathRoot = "./app/"
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz/", handler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
