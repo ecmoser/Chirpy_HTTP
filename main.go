85e38218323af01de820eae17de046adc890bcc0
ecmoser <78171469+ecmoser@users.noreply.github.com>
2025-05-08 13:44:15 -0400

    moved non-fileservers to /api/

--- main.go
+++ main.go
@@ -45,9 +45,9 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.Handle("GET /healthz", http.HandlerFunc(handlerHealthz))
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerHealthz)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
